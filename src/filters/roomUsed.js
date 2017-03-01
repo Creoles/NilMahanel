b5e03c33247fbb01dcb6c83328aa90aaf6dcf11f
rancongjie <95633676@qq.com>
2017-03-01 23:26:02 +0800

    valid vehicle

--- src/filters/roomUsed.js
+++ src/filters/roomUsed.js
@@ -1,16 +1,14 @@
 /**
  * Created by rancongjie@126.com on 17/2/27.
  */
-export default function(arr,option) {
-  let sum = 0;
-  arr.forEach(item=>{
-    sum += item.used;
-  });
-  if (option){
-    debugger;
-    return Math.floor(sum/option);
-  }else {
-    debugger;
-    return sum
-  }
+export default function(arr, option) {
+    let sum = 0;
+    arr.forEach(item => {
+        sum += item.used;
+    });
+    if (option) {
+        return Math.floor(sum / option);
+    } else {
+        return sum
+    }
 }
--- src/views/admin/manager/resource/vehicle/EditVehicle.vue
+++ src/views/admin/manager/resource/vehicle/EditVehicle.vue
@@ -1,52 +1,78 @@
 <template>
     <div>
         <content-top></content-top>
-        <el-form :model="params" label-position="top" class="vehicle-form">
-            <el-form-item label="公司名称">
+        <el-form :model="params" :inline="true" :rules="rule" label-position="top" class="vehicle-form">
+            <el-form-item label="国家" prop="country">
+                <el-select v-model="params.region" placeholder="Select">
+                    <el-option label="Srilanka" value="1"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="公司名称" style="width:400px" prop="name">
                 <el-input v-model="params.name"></el-input>
             </el-form-item>
-            <el-form-item label="车辆型号">
+            <el-form-item label="车辆型号" prop="model" class="width-class">
                 <el-input v-model="params.xinghao"></el-input>
             </el-form-item>
-            <el-form-item label="车牌号">
+            <el-form-item label="车牌号" prop="ID" class="width-class">
                 <el-input v-model="params.carId"></el-input>
             </el-form-item>
-            <el-form-item label="座位数">
+            <el-form-item label="座位数" prop="set" class="width-class">
                 <el-input v-model="params.zuoweishu"></el-input>
             </el-form-item>
-            <el-form-item label="年限">
+            <el-form-item label="年限" prop="year" class="width-class">
                 <el-input v-model="params.year"></el-input>
             </el-form-item>
-            <el-form-item label="保险号">
+            <el-form-item label="保险号" prop="baoxian" class="width-class">
                 <el-input v-model="params.year"></el-input>
             </el-form-item>
-            <el-form-item label="旅游局登记号">
+            <el-form-item label="旅游局登记号" prop="dengji" class="width-class">
                 <el-input v-model="params.year"></el-input>
             </el-form-item>
-            <el-form-item label="预定电话">
+            <el-form-item label="联系人" prop="people" class="width-class">
+                <el-input v-model="params.people"></el-input>
+            </el-form-item>
+            <el-form-item label="预定电话" prop="phone" class="width-class">
                 <el-input v-model="params.phone"></el-input>
             </el-form-item>
-            <el-form-item>
-                <el-button type="primary" v-if="!submitting">提 交</el-button>
-                <el-button type="primary" v-if="submitting" :loading="submitting">正在提交...</el-button>
+            <el-form-item label="价格/每公里(USD)" class="width-class" prop="price">
+                <el-input v-model="params.price"></el-input>
             </el-form-item>
         </el-form>
+        <el-button type="primary" v-if="!submitting">提 交</el-button>
+        <el-button type="primary" v-if="submitting" :loading="submitting">正在提交...</el-button>
     </div>
 </template>
 <style lang="scss">
     .vehicle-form {
-        width:500px;
+        width:725px;
+        .width-class {
+            width:192px;
+        }
     }
 </style>
 <script>
     import ContentTop from "../../../../components/ContentTop.vue"
+    import editRule from "../../../../../assets/valid/editVehicle.json"
     export default{
         data(){
             return{
                 params:{
 
-                }
+                },
+                rule:editRule
 
+            }
+        },
+        created(){
+            this.rule.price.push({ validator: this.validPrice, trigger: 'blur' });
+        },
+        methods:{
+            validPrice(rule,value,callback){
+                var bool = /^-?[1-9]+(\.\d+)?$|^-?0(\.\d+)?$|^-?[1-9]+[0-9]*(\.\d+)?$/.test(value);
+                if (!bool){
+                    callback(new Error('必须为数字,可以有小数'));
+                }
+                callback();
             }
         },
         components:{
