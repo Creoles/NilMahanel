41d8ddfeed7cb77062d8408eec34c7c553ddd50c
rancongjie <95633676@qq.com>
2017-03-06 09:22:20 +0800

    hotel change

--- src/views/admin/manager/resource/shop/EditShop.vue
+++ src/views/admin/manager/resource/shop/EditShop.vue
@@ -2,9 +2,6 @@
     <div>
         <content-top></content-top>
         <el-form :model="params" :rules="rule" label-position="top" class="shop-form">
-            <el-form-item label="商店名称" prop="name">
-                <el-input v-model="params.name"></el-input>
-            </el-form-item>
             <el-form-item label="国家" class="inline select" prop="country">
                 <el-select v-model="params.country" class="restaurant-region" placeholder="select">
                     <el-option label="1" value="Srilanka"></el-option>
@@ -16,16 +13,22 @@
                     <el-option label="2" value="beijing"></el-option>
                 </el-select>
             </el-form-item>
-            <el-form-item label="商店地址" prop="address">
-                <el-input v-model="params.address"></el-input>
-            </el-form-item>
             <el-form-item label="类型" class="inline select" prop="type">
                 <el-select v-model="params.type" class="restaurant-region" placeholder="select">
-                    <el-option label="1" value="中餐"></el-option>
-                    <el-option label="2" value="西餐"></el-option>
-                    <el-option label="3" value="特色"></el-option>
-                    <el-option label="4" value="综合"></el-option>
+                    <el-option label="中餐" value="1"></el-option>
+                    <el-option label="西餐" value="2"></el-option>
+                    <el-option label="特色" value="3"></el-option>
+                    <el-option label="综合" value="4"></el-option>
                 </el-select>
+            </el-form-item>
+            <el-form-item label="中文店名" prop="name" class="inline-input">
+                <el-input v-model="params.name"></el-input>
+            </el-form-item>
+            <el-form-item label="英文店名" prop="name" class="inline-input">
+                <el-input v-model="params.name"></el-input>
+            </el-form-item>
+            <el-form-item label="商店地址" prop="address" class="inline-input">
+                <el-input v-model="params.address"></el-input>
             </el-form-item>
             <el-form-item label="联系人员" class="inline contact" prop="people">
                 <el-input v-model="params.people"></el-input>
@@ -33,7 +36,14 @@
             <el-form-item label="联系电话" class="inline contact" prop="phone">
                 <el-input v-model="params.phone"></el-input>
             </el-form-item>
-            <el-form-item label="商店介绍">
+            <el-form-item label="中文介绍" class="inline-input">
+                <el-input
+                        type="textarea"
+                        autosize
+                        v-model="params.textarea">
+                </el-input>
+            </el-form-item>
+            <el-form-item label="英文介绍" class="inline-input">
                 <el-input
                         type="textarea"
                         autosize
@@ -68,9 +78,12 @@
 </template>
 <style lang="scss">
     .shop-form {
-        width: 500px;
         .inline {
             display: inline-block;
+        }
+        .inline-input {
+            display:inline-block;
+            width:500px;
         }
     }
 </style>
