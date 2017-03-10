fcc5153e3a115f705e5e2642eb18e66d621b5669
rancongjie <95633676@qq.com>
2017-03-10 14:26:04 +0800

    country crud

--- src/views/admin/manager/Country.vue
+++ src/views/admin/manager/Country.vue
@@ -1,14 +1,15 @@
 <template>
     <div>
-        <content-top><el-button class="addCountry" @click="addCountry" type="primary">添加国家</el-button></content-top>
+        <content-top><el-button class="addCountry" @click="dialogCountry = true" type="primary">添加国家</el-button></content-top>
         <el-collapse accordion>
-          <el-collapse-item v-for="item in country" :key="item.id">
+          <el-collapse-item v-for="item in countryList" :key="item.id">
             <template slot="title">
-              {{item.country}} <el-button type="text" @click="addCity($event)">添加城市</el-button>
+              {{item.name}}/{{item.name_en}} <el-button type="text" @click="openCityModel(item)">添加城市</el-button>
             </template>
-            <el-table :data="item.city" max-height="250">
+            <el-table :data="item.city_data" max-height="250">
               <el-table-column label="ID" prop="id"></el-table-column>
-              <el-table-column label="地区" prop="name"></el-table-column>
+              <el-table-column label="城市中文名称" prop="name"></el-table-column>
+              <el-table-column label="城市英文名称" prop="name_en"></el-table-column>
               <el-table-column label="操作">
                 <template scope="scope">
                   <el-button type="text" size="small" @click="removeCity(scope.
@@ -18,6 +19,34 @@
             </el-table>
           </el-collapse-item>
         </el-collapse>
+        <el-dialog title="添加国家" v-model="dialogCountry" size="tiny">
+          <el-form :model="countryModel">
+            <el-form-item label="中文名称">
+              <el-input v-model="countryModel.name" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="英文名称">
+              <el-input v-model="countryModel.name_en" auto-complete="off"></el-input>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogCountry = false">取 消</el-button>
+            <el-button type="primary" @click="addCountry">确 定</el-button>
+          </div>
+        </el-dialog>
+        <el-dialog title="添加城市" v-model="dialogCity" size="tiny">
+          <el-form :model="cityModel">
+            <el-form-item label="中文名称">
+              <el-input v-model="cityModel.name" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="英文名称">
+              <el-input v-model="cityModel.name_en" auto-complete="off"></el-input>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogCity = false">取 消</el-button>
+            <el-button type="primary" @click="addCity">确 定</el-button>
+          </div>
+        </el-dialog>
     </div>
 </template>
 <style lang="scss">
@@ -30,36 +59,54 @@
     export default{
         data(){
             return{
-                country:[
-                  {
-                    id:1,
-                    country:'SriLanka',
-                    city:[
-                      {id:1,name:'科伦坡'}
-                    ]
-                  }
-                ]
+              countryList:[{id:1, name: '斯里兰卡',name_en:'Srilanka',city_data:[
+                {name:'科伦坡',name_en:'colloo'}
+              ]}],
+              dialogCountry:false,
+              dialogCity:false,
+              countryModel:{
+                name:'',
+                name_en:''
+              },
+              cityModel:{},
+              currentCountry:''
             }
         },
         methods:{
-          addCountry(){
-            this.$prompt('请输入国家名', '添加国家(中文/English)', {
-              confirmButtonText: '确定',
-              cancelButtonText: '取消',
-              inputErrorMessage: '格式不正确'
-            }).then(({ value }) => {
-              this.$http();
+          loadList(){
+            this.$http.get('/country/all').then(res=>{
+              if (res.data.code === 200){
+                this.countryList = res.data
+              }
+            },err => {
+              console.log(new Error(err))
             })
           },
-          addCity(event){
-            event.stopPropagation();
-            this.$prompt('请输入地区名','添加城市(中文/English)',{
-              confirmButtonText: '确定',
-              cancelButtonText: '取消',
-              inputErrorMessage: '格式不正确'
-            }).then(({ value }) => {
-              this.$http();
+          addCountry(){
+            this.$http.post('/country/create',this.countryModel).then(res=>{
+              if(res.data.code === 200){
+                dialogCountry = false;
+                this.countryModel = {};
+                this.loadList();
+              }
             })
+            
+          },
+          addCity(){
+            this.cityModel['country_id'] = this.currentCountry;
+            this.$http.post('/city/create',this.cityModel).then(res=>{
+              if(res.data.code === 200){
+                dialogCity = false;
+                this.countryModel = {};
+                this.loadList();
+              }
+            })
+            
+          },
+          openCityModel(item){
+            event.stopPropagation();
+            this.currentCountry = item.id;
+            this.dialogCity = true;
           },
           removeCity(index,row){
             this.$confirm('此操作将删除该地区, 是否继续?', '提示', {
@@ -67,7 +114,7 @@
               cancelButtonText: '取消',
               type: 'warning'
             }).then(() => {
-              row.splice(index,1);
+             //todo
               this.$message({
                 type: 'success',
                 message: '删除成功!'
